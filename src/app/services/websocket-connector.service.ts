7022144ecd7a1eb99a7355f0b5654d6b253e08d8
Carlo THeunissen <carlotheunissen96@gmail.com>
2018-10-30 23:51:05 +0100

    let websocket reconnect when server is down

--- src/app/services/websocket-connector.service.ts
+++ src/app/services/websocket-connector.service.ts
@@ -6,19 +6,34 @@ import {EventListener} from '@angular/core/src/debug/debug_node';
 })
 export class WebsocketConnectorService {
 
-  private tw : WebSocket;
+  private tw : WebSocket = null;
   constructor() {
+    this.connect();
+  }
 
-    this.tw  = new WebSocket("ws://localhost:8090/reservation/");
-    this.tw.onmessage = (message) => {
-      console.log(JSON.parse(message.data));
-      this.reservationUpdate.emit(JSON.parse(message.data));
-    };
-    this.tw.onopen = () => {
-      console.log("OPEN!");
-      var temp = {"roomId": 1};
-      this.tw.send(JSON.stringify(temp));
-    };
+  private connect() : void{
+    try {
+      if(this.tw === null || this.tw.readyState === this.tw.CLOSING || this.tw.readyState === this.tw.CLOSED) {
+        this.tw = new WebSocket("ws://localhost:8090/reservation/");
+        this.tw.onmessage = (message) => {
+          console.log(JSON.parse(message.data));
+          this.reservationUpdate.emit(JSON.parse(message.data));
+        };
+        this.tw.onopen = () => {
+          console.log("OPEN!");
+          let temp = {"roomId": 1};
+          this.tw.send(JSON.stringify(temp));
+        };
+        this.tw.onerror = () => {
+          this.connect();
+        };
+        this.tw.onclose = () => {
+          this.connect();
+        }
+      }
+    }catch (e){
+      setTimeout(() => this.connect(), 100);
+    }
   }
   public reservationUpdate = new EventEmitter();
 
