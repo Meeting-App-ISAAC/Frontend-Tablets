dd7eb18b7d488ae68c23de04a351d9e8ebbd816d
Carlo THeunissen <carlotheunissen96@gmail.com>
2018-11-06 12:55:54 +0100

    The reservation can only be extended the amount it is allowed to do so

--- src/app/home/occupied/occupied.component.ts
+++ src/app/home/occupied/occupied.component.ts
@@ -10,15 +10,51 @@ import {ReservationModel} from '../../interfaces/ReservationModel';
 export class OccupiedComponent implements OnInit {
 
   @Input() currentReservation : ReservationModel;
+  @Input() nextReservation : ReservationModel;
   @Input() until : Date;
   @Input() title : String;
   @Output() end = new EventEmitter();
   public timeButtonSelected : number = 0;
+  public timeButtonsFree : number = -1;
+
   public showDialog : boolean =false;
+  public error : boolean = false;
   public constructor(private rest : ReservationStatusRESTService){}
+  private setButtonFree(time : number){
+    this.timeButtonsFree = time;
+    if(this.timeButtonSelected > time){
+      this.timeButtonSelected = time;
+    }
+  }
   ngOnInit() {
+
+    if(!this.nextReservation){
+      this.setButtonFree(3);
+      return;
+    }
+
+    let timeUntilinMinutes =  (this.nextReservation.startHour - this.currentReservation.startHour + this.currentReservation.length) * 60;
+    if(timeUntilinMinutes < 15){
+      this.setButtonFree(-1);
+      return;
+    }
+    if(timeUntilinMinutes < 30){
+      this.setButtonFree(0);
+      return;
+    }
+    if(timeUntilinMinutes < 45){
+      this.setButtonFree(1);
+      return;
+    }
+    if(timeUntilinMinutes < 60){
+      this.setButtonFree(2);
+      return;
+    }
+    this.setButtonFree(3);
+
   }
   public endEvent() : void{
+    this.closeExtendDialog();
     this.end.emit();
   }
   public extendEvent() : void{
@@ -31,9 +67,28 @@ export class OccupiedComponent implements OnInit {
     this.showDialog = false;
     this.timeButtonSelected = 0;
   }
+  private showError(){
+    this.error = true;
+  }
+  private hideError(){
+    this.error = false;
+  }
   public submitExtendDialog() : void{
-    this.rest.sendReservationExtend(this.currentReservation, this.getMinutes());
-    this.closeExtendDialog();
+    this.hideError();
+    this.rest.sendReservationExtend(this.currentReservation, this.getMinutes()).subscribe(
+      (val) => {
+        if(val.toString() === "true"){
+          this.closeExtendDialog();
+        } else {
+          this.showError();
+        }
+      },
+      response => {
+        //POST call in error
+      },
+      () => {
+        //The POST observable is now completed
+      });
   }
   private getMinutes() : number{
     switch (this.timeButtonSelected){
